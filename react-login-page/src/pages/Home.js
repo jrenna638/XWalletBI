1148662817e8b4b39c5b6183b2cc93ee52796255
jrenna638 <jrenna@uade.edu.ar>
2024-05-16 10:28:33 -0300

    Home

--- react-login-page/src/pages/Home.js
+++ react-login-page/src/pages/Home.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
+import HeroSection from '../components/HeroSection';
+import Footer from '../components/Footer';
 
 function Home() {
-  return <h1>EMBEBER POWER BI AQUI</h1>;
+  const navbarLinks = [
+      { path: '/features', label: 'Características' },
+      { path: '/about', label: 'Acerca De' },
+      { path: '/contact', label: 'Contacto' }
+  ];
+
+  return (
+      <div>
+          <Navbar links={navbarLinks} />
+          <HeroSection 
+              title="Bienvenido a XWallet" 
+              subtitle="Gestiona tus finanzas con facilidad y seguridad." 
+              buttonText="Saber Más"
+          />
+          
+          <Footer />
+      </div>
+  );
 }
 
 export default Home;
